16cdde727be9dcc9cdf723aced6ac5fea765a1ff
Danilo Petrovic <danillo.messi@gmail.com>
2024-06-29 16:20:27 +0200

    opis

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -31,10 +31,6 @@ const Chat = () => {
   const endRef = useRef(null);
 
   useEffect(() => {
-    endRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, []);
-
-  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       if (currentUser) {
@@ -87,6 +83,10 @@ const Chat = () => {
     }
   }, [ownProfile, userFirestore]);
 
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [currentChat]);
+
   const handleEmoji = (e) => {
     formik.setFieldValue("message", formik.values.message + e.emoji);
     setIsOpen(false);
@@ -118,7 +118,7 @@ const Chat = () => {
         ];
 
         await updateDoc(chatDocRef, { messages: updatedMessageArr });
-        formik.values.message = "";
+        formik.resetForm(); // Reset form to clear the message input field
       }
     },
   });
@@ -180,11 +180,11 @@ const Chat = () => {
                   </div>
                 )
               )}
+              <div ref={endRef}></div>
             </div>
           ) : null}
         </div>
 
-        <div ref={endRef}></div>
         <form className="inputs-header-div" onSubmit={formik.handleSubmit}>
           <input
             name="message"
@@ -193,6 +193,11 @@ const Chat = () => {
             value={formik.values.message}
             placeholder="Enter your message..."
           />
+
+          <button type="submit" className="chat-send-button">
+            Send
+          </button>
+
           <div className="emoji-div">
             <button
               className="emoji-picker-button"
@@ -209,9 +214,6 @@ const Chat = () => {
               </div>
             )}
           </div>
-          <button type="submit" className="chat-send-button">
-            Send
-          </button>
         </form>
       </div>
     </div>
